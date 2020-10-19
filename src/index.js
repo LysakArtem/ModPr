d339a1691ae02adaa64db700cf3a74e063c3d163
LysakArtem <Perry.dev7@gmail.com>
2020-10-20 01:28:57 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import './styles.css';
+import imagesListTempl from './templates/images-list.hbs';
+import service from './js/service.js';
+const debounce = require('lodash.debounce');
+
+
+
+
+
+const inputRef = document.querySelector('.search-form');
+const imagesListRef = document.querySelector('.gallery');
+const btnloadMoreRef = document.querySelector('.btn-load-more');
+
+function renderMarkup(data) {
+    let markup = null;
+    markup = imagesListTempl(data.hits);
+    imagesListRef.insertAdjacentHTML('beforeend', markup);
+}
+
+// const debounceInputCallback = debounce(, 500);
+let heightScroll = document.documentElement.offsetHeight;//
+
+inputRef.addEventListener('input', (event) => {
+    service.query = event.target.value;
+    service.resetPage();
+    if(service.query){
+        service.fetchArticles().then(data => {
+            imagesListRef.innerHTML = '';
+            renderMarkup(data);
+            heightScroll = document.documentElement.offsetHeight;
+            if(data.total === 0 || service.remainingArticles <= 0){
+                btnloadMoreRef.classList.add('is-hidden');
+            }
+            else{
+                btnloadMoreRef.classList.remove('is-hidden');
+            }
+        });
+    } 
+    else { 
+        imagesListRef.innerHTML = '';
+        btnloadMoreRef.classList.add('is-hidden');
+        // return;- оставил, потому что будет вопрос(чтоб не забыть!)
+    };
+});
+
+btnloadMoreRef.addEventListener('click', () => {
+    service.fetchArticles().then(data => {
+        renderMarkup(data);
+        window.scrollTo({
+            top: heightScroll -70,   //Тоже вопрос!
+            behavior: 'smooth'
+        })
+        heightScroll = document.documentElement.offsetHeight
+        if(service.remainingArticles <= 0){
+            btnloadMoreRef.classList.add('is-hidden');
+        }
+        else{
+            btnloadMoreRef.classList.remove('is-hidden');
+        }
+    });
+});
---
+++ src/js/service.js
@@ -0,0 +1,29 @@
+const apiKey = '18602896-269905921176f8eb36b1925d1';
+
+export default {
+    inputValue: '',
+    page: 1, 
+    remainingArticles: null,
+    fetchArticles(){
+        const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.inputValue}&page=${this.page}&per_page=12&key=${apiKey}`;
+        console.log(url);
+        return fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(this.page);
+                this.remainingArticles = data.total - 12 * (this.page);
+                console.log(this.remainingArticles);
+                this.page += 1;
+                return data;
+            });
+    },
+    resetPage() {
+        return this.page = 1;
+    },
+    get query() {
+        return this.inputValue;
+    },
+    set query(value) {
+        return this.inputValue = value;
+    }     
+};
